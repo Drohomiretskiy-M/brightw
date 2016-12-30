45b6244358066e85c841694e69b8f7eab7982ac6
Misha Drohomiretskiy <drohomiretskiy@gmail.com>
2016-12-30 22:27:43 +0200

    Встановлено карту у футер (тестова)

--- www/wp-content/themes/brightw/footer.php
+++ www/wp-content/themes/brightw/footer.php
@@ -22,7 +22,7 @@
 <script src="https://code.jquery.com/jquery-1.12.1.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d84531.7522406103!2d24.96390479626599!3d48.54056625874349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4736d2957ef4cbdf%3A0xc1e2a9b84387bf00!2z0JrQvtC70L7QvNGL0Y8sINCY0LLQsNC90L4t0KTRgNCw0L3QutC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjA!5e0!3m2!1sru!2sua!4v1483129501457" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 <?php wp_footer(); ?>
 
