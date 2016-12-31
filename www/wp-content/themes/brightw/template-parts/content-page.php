510b7c9485499cc48102b3461f0cb4ff8e2b5678
Misha Drohomiretskiy <drohomiretskiy@gmail.com>
2016-12-31 11:48:49 +0200

    Попередньо заповнені сторінки /про мене/ контакти/ відео/

--- www/wp-content/themes/brightw/template-parts/content-page.php
+++ www/wp-content/themes/brightw/template-parts/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<!--		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>-->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -27,6 +27,7 @@
 
 	<?php if ( get_edit_post_link() ) : ?>
 		<footer class="entry-footer">
+<!--
 			<?php
 				edit_post_link(
 					sprintf(
@@ -38,6 +39,7 @@
 					'</span>'
 				);
 			?>
+-->
 		</footer><!-- .entry-footer -->
 	<?php endif; ?>
 </article><!-- #post-## -->
